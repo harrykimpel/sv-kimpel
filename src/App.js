e94682a34cd9a7d40491d99c3467234327f8756f
Harry Kimpel <harrykimpel@hotmail.com>
2020-06-24 16:58:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import background from './background.png'
 
 function App() {
   const value = 'Sachverständiger Thomas Kimpel';
-  return <div style={{ backgroundImage:`url(${background})` }}>Willkommen bei {value}</div>;
+  return 
+    <div style={{ width:`1024px`, height:`768px`, heightbackgroundImage:`url(${background})` }}>
+      <div style={{ marginLeft:`300px`, marginTop:`200px` }}>Willkommen bei {value}</div>
+    </div>;
 }
 
 export default App;
