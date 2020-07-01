e746f57889b17c849151c4b9dd95ca21c773138c
Harry Kimpel <harrykimpel@hotmail.com>
2020-07-01 14:22:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import background from './background.png'
+import testmark from './TR-Testmark_0000074165_DE_CMYK_with-QR-Code.png'
+import europacenter from './Ansicht_070207_Europacenter_2.png'
 
 function App() {
   const value = 'Sachverständiger Thomas Kimpel';
-  return <div style={{ width: `1024px`, height: `768px`, backgroundImage: `url(${background})` }}>
-      <div style={{ position: 'absolute', marginLeft: `450px`, marginTop: `200px`, color: `#ffffff`, fontWeight: 'bold' }}>Willkommen bei {value}</div>
-    </div>;
+  
+  const styleMain = {
+            width: `100%`,
+            height: `100%`,
+            backgroundImage: `url(${europacenter})`
+        }
+
+  const styleMainContent = {
+            position: 'absolute',
+            marginLeft: `620px`,
+            marginTop: `100px`,
+            color: `#ffffff`,
+            fontWeight: 'bold',
+            backgroundColor: "blue",
+            padding: '30px'
+        }
+
+  const styleTestMark = {
+            width: `400px`,
+            height: `148px`
+        }
+
+  const styleLinks = {
+            color: `#ffffff`,
+            fontWeight: 'bold',
+        }
+        
+  return <div style={styleMain}>
+            <div style={styleMainContent}>
+                Sachverständigen- und Ingenieurbüro Thomas Kimpel<br />
+                Dipl.-Bauingenieur (FH)<br />
+                Sachverständiger für Schäden an Gebäuden (TÜV)<br />
+                <br /><br />
+                <u>Leistungen:</u>
+                <ul>
+                    <li>Erstellen von Gutachten</li>
+                    <li>Bauzustandsbewertung</li>
+                    <li>Baubegleitende Qualitätskontrolle</li>
+                    <li>Durchsetzung von Mangel- / Gewährleistungsansprüchen</li>
+                    <li>Begehung vor Hauskauf</li>
+                    <li>Technische Prüfung/Bewertung von Bauverträgen</li>
+                    <li>Begleitung bei Abnahmen</li>
+                </ul>
+                <br /><br />
+                <u>Kontakt:</u><br />
+                Thomas Kimpel<br />
+                Bürgerhofweg 10<br />
+                36148 Kalbach<br />
+                Telefon: 09742 / 932994<br />
+                Mobil: 0176 / 12348025<br />
+                E-Mail: <a href='mailto:kimpel@sv-kimpel.de' style={styleLinks}>kimpel@sv-kimpel.de</a><br />
+                <br /><br />
+                Mitglied im BVS <a href='https://www.bvs-ev.de' style={styleLinks}>www.bvs-ev.de</a> (Anwärter)<br />
+                <img src={(testmark)} style={styleTestMark} />
+            </div>
+        </div>;
 }
 
 export default App;
